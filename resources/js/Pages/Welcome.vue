7fe07726320d7f9b6c73bdd9ae216f6091f1e158
Md Ismail Hossain <ihsojib43@gmail.com>
2024-06-04 22:52:04 +0600

    tailwind and prime vue installed

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div class="mt-10">
+        <Message severity="success">Welcome to Laravel 11 Vue Js 3 Interia Js and Tailwind Prime Vue starter! You can now customize this according to your need! Laravel Breeze Also Installed Here!</Message>
+</div>
+
+</template>
+
+<script>
+import Message from 'primevue/message';
+
+export default {
+    components: {
+        Message
+    },
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,32 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Lara from '@/presets/lara';
+
+import 'primeicons/primeicons.css'
+
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(PrimeVue, {
+                unstyled: true,
+                pt: Lara                            //apply preset
+            })
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
